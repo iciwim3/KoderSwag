2eabd292ed906bb25c77f31255fca6aa4953ffc8
SKE <appybuildmore@gmail.com>
2017-12-29 10:54:59 -0600

    Add Collection View and custom cell for the Product page in the app. Created the IBOutlet connections for the productImage, productTitle, and productPrice in the customProductCell file.

---
+++ koder-swag-1/koder-swag-1/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  koder-swag-1
+//
+//  Created by Sain-R Edwards Jr. on 12/29/17.
+//  Copyright © 2017 Appybuildmore. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
