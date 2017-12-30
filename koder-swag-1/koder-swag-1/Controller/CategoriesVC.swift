6419886b5d52b9b1d5be5480ef30d5b06d92a0ea
SKE <appybuildmore@gmail.com>
2017-12-30 16:31:38 -0600

    Updated the data models and controllers to display the correct info when user taps on the images. Also discovered the proper way to implement the correct titles on the Nav bar for the current product category.

--- koder-swag-1/koder-swag-1/Controller/CategoriesVC.swift
+++ koder-swag-1/koder-swag-1/Controller/CategoriesVC.swift
@@ -31,6 +31,22 @@ class CategoriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource
             return CategoryCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            // The assertion is used for Build time. If it doesn't work, it will force crash the app.
+            assert(sender as? Category != nil)
+            productsVC.initializeProducts(category: sender as! Category)
+        }
+    }
 
 }
 
---
+++ koder-swag-1/koder-swag-1/Controller/ProductsVC.swift
@@ -0,0 +1,45 @@
+//
+//  ProductsVC.swift
+//  koder-swag-1
+//
+//  Created by Sain-R Edwards Jr. on 12/30/17.
+//  Copyright © 2017 Appybuildmore. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+        
+    }
+
+    func initializeProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        
+        return ProductCell()
+    }
+    
+    
+}
